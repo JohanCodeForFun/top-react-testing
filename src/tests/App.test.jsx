aa094d08383678a3d22ce601376dcfaeb334a9e2
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-08-26 21:35:26 +0200

    remove broken beforeEach, update title & snapshot

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -3,17 +3,15 @@ import { describe, it, expect, beforeEach } from "vitest";
 import userEvent from "@testing-library/user-event";
 import App from "../App";
 
-beforeEach(() => {
-  render(<App />);
-
   describe("App component", () => {
     it("renders magnificent monkeys", () => {
       // since screen does not have the container property, we'll destructure render to obtain a container for this test
-      const { container } = <App />;
+      const { container } = render(<App />);
       expect(container).toMatchSnapshot();
     });
 
     it("renders radical rhinos after button click", async () => {
+      render(<App />);
       const user = userEvent.setup();
 
       const button = screen.getByRole("button", { name: "Click Me" });
@@ -25,7 +23,6 @@ beforeEach(() => {
       );
     });
   });
-});
 
 describe("something truthy and falsy", () => {
   it("true to be true", () => {

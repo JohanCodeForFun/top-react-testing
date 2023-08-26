161f51d4154f12271ad1c501e482926de80a553c
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-08-26 22:04:35 +0200

    WIP add button test

---
+++ src/tests/CustomButton.test.jsx
@@ -0,0 +1,10 @@
+import { describe, it, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { UserEvent } from "@testing-library/user-event";
+import CustomButton from "../components/CustomButton";
+
+describe('CustomButton', () => {
+  it('should render a button with the text "Click me"'), () => {
+
+  }
+})
